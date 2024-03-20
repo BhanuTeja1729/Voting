c7292f04254ec2af00d745d1aaa9266549fc0920
Shashank K Prabhu <shashankkp02@gmail.com>
2024-03-20 23:27:55 +0530

    Installed thirdweb sdk and login functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThirdwebProvider, embeddedWalletConfig } from "thirdweb/react";
+import { ThirdwebProvider } from "../constants/thirdweb.js";
+import { client } from "../constants/client.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThirdwebProvider
-  client={import.meta.env.REACT_APP_THIRDWEB_CLIENT}
-  wallets={[
-    embeddedWalletConfig(),
-  ]}
+  <ThirdwebProvider 
+    client={client}
   >
     <App />
   </ThirdwebProvider>
