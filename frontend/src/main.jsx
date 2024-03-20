40b8618a417842ea1eb1fe818b8fe0b51b248878
Shashank K Prabhu <shashankkp02@gmail.com>
2024-03-20 21:55:41 +0530

    Installed web3 and tries connect wallet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThirdwebProvider, embeddedWalletConfig } from "thirdweb/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ThirdwebProvider
+  client={import.meta.env.REACT_APP_THIRDWEB_CLIENT}
+  wallets={[
+    embeddedWalletConfig(),
+  ]}
+  >
     <App />
-  </React.StrictMode>
+  </ThirdwebProvider>
 );
