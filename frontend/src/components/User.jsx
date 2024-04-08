61534bb505b0aed123c81fa27d05ddb759a1467e
Murali Kulkarni <muralikulkarni14@gmail.com>
2024-04-08 23:06:48 +0530

    did some styling to fix some issues

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -7,7 +7,16 @@ const User = () => {
   let location = useLocation();
   return (
     <>
-      {location.pathname === "/user/login" ? (
+      {location.pathname === "/user/guidelines" ? (
+        <div>
+          <div className="ml-0">
+            <Sidebar />
+          </div>
+          <Box sx={{ ml: 40, mt: 10 }}>
+            <Outlet />
+          </Box>
+        </div>
+      ) : (
         <div className="flex justify-center items-center">
           <div className="container w-full mt-20 flex flex-col md:flex-row bg-white shadow-md rounded-xl px-5 py-5 ">
             <div className="flex-1">
@@ -19,20 +28,11 @@ const User = () => {
             </div>
 
             <div className="flex-1 p-8 flex justify-center items-center bg-gray">
-              <Box sx={{ ml: 35 }}>
+              <Box sx={{}}>
                 <Outlet />
               </Box>
             </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <div className="ml-0">
-            <Sidebar />
-          </div>
-          <Box sx={{ ml: 45, mt: 10 }}>
-            <Outlet />
-          </Box>
         </div>
       )}
     </>
