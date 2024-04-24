f0dbed64cc6cfc98785de9e93a67a203f31a79ca
Shashank K Prabhu <shashankkp02@gmail.com>
2024-04-24 19:55:46 +0530

    Added candidates to blockchain

--- frontend/src/components/admin/candidate.jsx
+++ frontend/src/components/admin/candidate.jsx
@@ -22,9 +22,18 @@ import { create } from "../../api/admin";
 
 import CandidateCard from "./candidateCard";
 
+//contracts
+import { prepareContractCall, sendTransaction, resolveMethod } from "thirdweb";
+import {
+  useActiveAccount,
+  // useSendTransaction,
+} from "thirdweb/react";
+import candContract from "../../contracts/candidate";
+
 const candidate = () => {
   const adminContext = useContext(AdminContext);
   const { candidateList, getCandidateList } = adminContext;
+  const account = useActiveAccount();
 
   useEffect(() => {
     getCandidateList();
@@ -47,27 +56,48 @@ const candidate = () => {
         candidateFirstName,
         candidateLastName
       );
-      try {
-        const res = await create(
-          candidateFirstName,
-          candidateLastName,
-          wardNo,
-          party,
-          imgUrl
-        );
-        console.log(res);
-        if (res.error) {
-          console.error(res.error);
-        } else {
-          console.log(res.message);
-          //Refresh Page
-          window.location.reload();
+      const _name = candidateFirstName + " " + candidateLastName;
+      const _election_id = 1;
+      const _ward_no = wardNo;
+      const _imgURL = imgUrl;
+      const _party = party;
+      const transaction = await prepareContractCall({
+        contract: candContract,
+        method: resolveMethod("addCandidate"),
+        params: [_name, _election_id, _ward_no, _imgURL, _party],
+      });
+      const { transactionHash } = await sendTransaction({
+        transaction,
+        account,
+      });
+      console.log(transactionHash);
+      if (transactionHash) {
+        try {
+          const res = await create(
+            candidateFirstName,
+            candidateLastName,
+            wardNo,
+            party,
+            imgUrl
+          );
+          console.log(res);
+          if (res.error) {
+            console.error(res.error);
+          } else {
+            console.log(res.message);
+            //Refresh Page
+            getCandidateList();
+          }
+        } catch (error) {
+          console.error("Candidate Not Registered on database");
+          console.error(error);
         }
-      } catch (error) {
-        console.error(error);
       }
     } catch (error) {
-      alert(error);
+      console.log(error);
+      console.log(
+        "Candidate Not Registered either blockchain error or cloudinary"
+      );
     }
   };
 
@@ -104,86 +134,90 @@ const candidate = () => {
         sx={{ mx: 5, mt: 12, mb: 2, overflow: "hidden", maxHeight: "100vh" }}
       >
         {/* <h1 className="mb-8 text-3xl font-semibold">Candidate</h1> */}
-        <div className="shadow w-4/5">
-          <Accordion defaultExpanded>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel3-content"
-              id="panel3-header"
-            >
-              <div className="text-3xl font-semibold">Register Candidates</div>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
-                <TextField
-                  type="text"
-                  variant="outlined"
-                  color="secondary"
-                  label="First Name"
-                  onChange={(e) => setCandidateFirstName(e.target.value)}
-                  value={candidateFirstName}
-                  fullWidth
-                  required
-                />
-                <TextField
-                  type="text"
-                  variant="outlined"
-                  color="secondary"
-                  label="Last Name"
-                  onChange={(e) => setCandidateLastName(e.target.value)}
-                  value={candidateLastName}
-                  fullWidth
-                  required
-                />
-              </Stack>
-              <TextField
-                type="text"
-                variant="outlined"
-                color="secondary"
-                label="Ward. No"
-                onChange={(e) => setWardNo(e.target.value)}
-                value={wardNo}
-                fullWidth
-                required
-                sx={{ mb: 4 }}
-              />
-              <Box sx={{ mb: 3 }}>
-                <FormControl fullWidth>
-                  <InputLabel>Party</InputLabel>
-                  <Select
-                    value={party}
-                    label="Party"
-                    onChange={(e) => setParty(e.target.value)}
-                  >
-                    {options.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Box>
-              <Box sx={{ justifyContent: "space-between", display: "flex" }}>
-                <Button variant="contained" component="label">
-                  Upload File
-                  <input
-                    type="file"
-                    hidden
-                    onChange={(e) => setImg(e.target.files[0])}
+        {account && (
+          <div className="shadow w-4/5">
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3-content"
+                id="panel3-header"
+              >
+                <div className="text-3xl font-semibold">
+                  Register Candidates
+                </div>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
+                  <TextField
+                    type="text"
+                    variant="outlined"
+                    color="secondary"
+                    label="First Name"
+                    onChange={(e) => setCandidateFirstName(e.target.value)}
+                    value={candidateFirstName}
+                    fullWidth
+                    required
                   />
-                </Button>
-                <Button
-                  variant="contained"
-                  color="success"
-                  sx={{ mr: 1.5 }}
-                  onClick={handleRegister}
-                >
-                  Register
-                </Button>
-              </Box>
-            </AccordionDetails>
-          </Accordion>
-        </div>
+                  <TextField
+                    type="text"
+                    variant="outlined"
+                    color="secondary"
+                    label="Last Name"
+                    onChange={(e) => setCandidateLastName(e.target.value)}
+                    value={candidateLastName}
+                    fullWidth
+                    required
+                  />
+                </Stack>
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  color="secondary"
+                  label="Ward. No"
+                  onChange={(e) => setWardNo(e.target.value)}
+                  value={wardNo}
+                  fullWidth
+                  required
+                  sx={{ mb: 4 }}
+                />
+                <Box sx={{ mb: 3 }}>
+                  <FormControl fullWidth>
+                    <InputLabel>Party</InputLabel>
+                    <Select
+                      value={party}
+                      label="Party"
+                      onChange={(e) => setParty(e.target.value)}
+                    >
+                      {options.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box sx={{ justifyContent: "space-between", display: "flex" }}>
+                  <Button variant="contained" component="label">
+                    Upload File
+                    <input
+                      type="file"
+                      hidden
+                      onChange={(e) => setImg(e.target.files[0])}
+                    />
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="success"
+                    sx={{ mr: 1.5 }}
+                    onClick={handleRegister}
+                  >
+                    Register
+                  </Button>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        )}
         <div className="ml-5 my-5 text-3xl font-semibold">
           <Typography variant="">Registered Candidates</Typography>
         </div>
